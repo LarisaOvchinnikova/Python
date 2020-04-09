1d6a1576c1300d62d2f3a65f7039f828aba238b5
Larisa <larisaplantation@gmail.com>
2020-04-08 23:00:57 -0400

    add lesson1.py

---
+++ lesson1.py
@@ -0,0 +1,11 @@
+name = input("What is your name")
+print("Hello", name + "!")
+
+age = float(input("Enter your age"))
+print("Hi", age, "is the best")
+
+number = float(input("Enter number"))
+print(round(number,1))
+
+
+
