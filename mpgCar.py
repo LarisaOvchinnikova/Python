ed49c5f855ad9cc3a0addfb3f2d69903e242d5b2
Larisa <larisaplantation@gmail.com>
2020-04-09 00:19:11 -0400

    add work with string

--- mpgCar.py
+++ mpgCar.py
@@ -1,5 +1,5 @@
 tank = float(input("How many gallons? "))
-mpg = float(input("What is miles per gallon? "))
+mpg = float(input("What's miles per gallon? "))
 miles = tank * mpg
 print("You can drive", miles, "miles")
 price = float(input("Print price per gallon"))
@@ -10,4 +10,5 @@ if miles < distance:
     print("you can not get gas station ")
 else: print("you have enough gas")
 print(f"You can drive {miles} and cost will be {price*tank} dollars")
+
 
