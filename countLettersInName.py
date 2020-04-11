9b058c861489d03b39939191bf77788f9217ecef
Larisa <larisaplantation@gmail.com>
2020-04-11 17:42:04 -0400

    fixed

--- countLettersInName.py
+++ countLettersInName.py
@@ -23,7 +23,7 @@ print(f"{firstName.rjust(m)}\n{middleName.rjust(m)}\n{lastName.rjust(m)}")
 
 # Strip the string of first 3 and last 3 characters
 word2 = input("Enter word (length > 6): ")
-if len(word) < 7:
+if len(word2) < 7:
     print("Too short word for this task")
 else:
     print(word2[3:-3])
