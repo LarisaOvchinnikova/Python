3f1b454b58ada8229ba6d0ea3f366eeb04637457
Larisa <larisaplantation@gmail.com>
2020-04-11 19:11:47 -0400

    fixed

--- word in box.py
+++ word in box.py
@@ -8,5 +8,12 @@ else:
     print("*" + " "*space + word + " "*space + "*")
     print("*"*width)
 
+# 2 case:
 
-
+if width < len(word):
+    print("ERROR")
+else:
+    space = int((width - len(word))/2) - 1
+    print("*" * width)
+    print("*" + word.center(width - 2) + "*")
+    print("*"*width)
