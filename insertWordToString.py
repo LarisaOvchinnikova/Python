89703324705d0a1363bf047176666b3bb7e94973
Larisa <larisaplantation@gmail.com>
2020-04-11 23:25:04 -0400

    add task

--- insertWordToString.py
+++
@@ -1,6 +0,0 @@
-# Insert second string in the middle of the first string
-first = input("input string")
-second = input("input word to insert in string")
-i = len(first) // 2
-print(f"New string: {first[0:i]+second+first[i:]}")
-
