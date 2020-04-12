39f13206298d4266cba65157071a5cc2c4e0f016
Larisa <larisaplantation@gmail.com>
2020-04-12 11:51:55 -0400

    add codewars solution

--- codewars python/Return Negative.py
+++ codewars python/Return Negative.py
@@ -1,8 +1,10 @@
 # Return Negative
 # https://www.codewars.com/kata/55685cd7ad70877c23000102
 def make_negative(number):
-   if number < 0: return number
-   else: return -number
+   if number < 0:
+      return number
+   else:
+      return -number
 
 
 # 2 case
