a723fb85c3ef454c604e188086dae3f6220fc0fa
Larisa <larisaplantation@gmail.com>
2020-04-13 23:04:19 -0400

    add task

--- HW3/5 - Check for a Vowel.py
+++ HW3/5 - Check for a Vowel.py
@@ -9,6 +9,8 @@ else:
     print("Your name starts with consonant")
 # 2 case:
 vowel = 'aeuio'
+if " " in name:
+    print("Enter only one name")
 if name[0].lower() in vowel:
     print("Your name starts with vowel")
 else:
--- HW3/7 -properties of a string.py
+++ HW3/7 -properties of a string.py
@@ -15,3 +15,20 @@ elif word.isalnum():    # letters and digits
     print(f"word \"{word}\" has all kinds of stuff")
 
 
+if word.isdigit():
+    print(f"word \"{word}\" has only digits")
+elif word.isalpha():
+    if word.islower():
+        print("Only lowercase")
+    elif word.isupper():
+        print("hase onle uppercase")
+    else:
+        print("yas lowercase and uppercase")
+elif word.isalnum():
+    if word.islower():
+        print("Has only lowercase and digits")
+    elif word.isupper():
+        print ("has only uppercase and digits" )
+    else: print("has upper and lower and digits")
+
+
