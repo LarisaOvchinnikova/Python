7ef64ac4231d9cdc53f32be41a90d51256d97c0c
Larisa <larisaplantation@gmail.com>
2020-04-13 22:50:03 -0400

    add task

--- HW3/5 - Check for a Vowel.py
+++ HW3/5 - Check for a Vowel.py
@@ -7,4 +7,10 @@ elif name[0] in vowels:
     print("Your name starts with vowel")
 else:
     print("Your name starts with consonant")
+# 2 case:
+vowel = 'aeuio'
+if name[0].lower() in vowel:
+    print("Your name starts with vowel")
+else:
+    print("Your name starts with consonant")
 
