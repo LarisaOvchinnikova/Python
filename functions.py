c7589a9f99c0e02b060057ab5f1ddf43193da76e
Larisa <larisaplantation@gmail.com>
2020-04-14 00:30:32 -0400

    add task

--- functions.py
+++ functions.py
@@ -11,3 +11,21 @@ def minimum(a,b):
         return b
 
 print(minimum (5, 1))
+
+def sum(a, b, c):
+    return a + b + c
+
+print(sum(1,2,3))
+
+def convert(f):
+    c = (f - 32) * 5 / 9
+    return round(c, 2)
+print(f"today is {convert(100)} degrees")
+# ----------------------------------
+arr = [1, 2, 3]
+def func():
+    arr.append(4)            # change global array
+
+func()
+print(arr)                   # print [1, 2, 3, 4]
+# ----------------------------------
