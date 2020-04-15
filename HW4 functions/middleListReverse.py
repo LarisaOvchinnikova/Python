283a6fbe69b7a44848b65b9c6e17ac83d4a1d506
Larisa <larisaplantation@gmail.com>
2020-04-14 23:44:03 -0400

    middle of list.py

---
+++ HW4 functions/middleListReverse.py
@@ -0,0 +1,8 @@
+def middleOfList(input):
+    k = len(input)
+    if k % 3 != 0:
+        return "Error"
+    else:
+        p = int(k / 3)
+        return input[p: p+p][::-1]
+print(middleOfList([1,2,3,4,5,6,7,8, 9]))
