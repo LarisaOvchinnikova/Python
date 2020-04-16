fd1d2b73f43970953a47437d0068a793d9a4bb74
Larisa <larisaplantation@gmail.com>
2020-04-15 22:46:49 -0400

    get_us_time.py

--- HW4 functions/middleListReverse.py
+++ HW4 functions/middleListReverse.py
@@ -3,6 +3,8 @@ def middleOfList(input):
     if k % 3 != 0:
         return "Error"
     else:
-        p = int(k / 3)
-        return input[p: p+p][::-1]
+        p = int(k / 3)   # p = k // 3
+        middle = input[p: -p]
+        return middle[::-1]
+        # return input[p: p+p][::-1]
 print(middleOfList([1,2,3,4,5,6,7,8, 9]))
