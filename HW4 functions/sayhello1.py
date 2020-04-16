5934a34f139260bf06d97e8cc2f037053be60aae
Larisa <larisaplantation@gmail.com>
2020-04-15 22:43:35 -0400

    get_us_time.py

--- HW4 functions/sayhello1.py
+++ HW4 functions/sayhello1.py
@@ -1,7 +1,7 @@
 def sayhello(name, hour):
-    if hour >= 0 and hour <= 6:
+    if hour >= 0 and hour <= 6:  # if hour in range(0,7):
         time = 'night'
-    elif hour >= 7 and hour <= 11:
+    elif hour >= 7 and hour <= 11: # if hour in range(7, 12):
         time = 'morning'
     elif hour >= 12 and hour <= 17:
         time = 'day'
