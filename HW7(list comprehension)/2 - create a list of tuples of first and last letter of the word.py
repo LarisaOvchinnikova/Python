3b93b02881cb36d134af1140f974fe2142175de6
Larisa <larisaplantation@gmail.com>
2020-04-22 18:38:33 -0400

    count palindromes in range

--- HW7(list comprehension)/2 - create a list of tuples of first and last letter of the word.py
+++ HW7(list comprehension)/2 - create a list of tuples of first and last letter of the word.py
@@ -2,5 +2,5 @@
 # of first and last letter of the word
 
 input = ['Apricot', 'Cat', 'Dog', 'Ocelot', 'Zebra', 'Bat', 'Orange']
-output = [(word[0], word[len(word)-1]) for word in input]
+output = [(word[0], word[-1]) for word in input]
 print(output)
