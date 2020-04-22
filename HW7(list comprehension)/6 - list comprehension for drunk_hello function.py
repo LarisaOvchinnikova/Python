dab8c3c4a72df848462b819d9518f3ba4fa708fa
Larisa <larisaplantation@gmail.com>
2020-04-22 00:04:42 -0400

    a list comprehension for drunk_hello function

--- HW7(list comprehension)/6 - list comprehension for drunk_hello function.py
+++ HW7(list comprehension)/6 - list comprehension for drunk_hello function.py
@@ -1,6 +1,6 @@
 # Write a list comprehension for drunk_hello function
 def drunk_hello(text):
-    return [letter if i % 2 != 0 else letter.upper() for i, letter in enumerate(text)]
+    return "".join([letter if i % 2 != 0 else letter.upper() for i, letter in enumerate(text)])
 
 text = "sky is blue"
 print(drunk_hello(text))
