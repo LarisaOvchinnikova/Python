90e3bc8544c6220e943395b6a2064c5bf76d3dfd
Larisa <larisaplantation@gmail.com>
2020-04-21 23:35:48 -0400

    x and y coordinates

---
+++ HW7(list comprehension)/4 - X and Y Coordinates.py
@@ -0,0 +1,12 @@
+# X and Y Coordinates
+def convert_cartesian(x, y):
+    lst = []
+    for i in range(len(x)):
+        lst.append((x[i], y[i]))
+    return lst
+
+print(convert_cartesian([1, 5, 3, 3, 4], [5, 8, 9, 1, 0]) )
+x = [1,5,3,3,4]
+y = [5,8,9,1,0]
+lst = [(el, el1) for el in x  for el1 in y]
+print(lst)
