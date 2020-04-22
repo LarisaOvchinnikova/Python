1319e4b54bb9b5818c2fdbbc041ea7e496aa6c11
Larisa <larisaplantation@gmail.com>
2020-04-21 23:11:01 -0400

    list of tuples

---
+++ HW7(list comprehension)/2 - create a list of tuples of first and last letter of the word.py
@@ -0,0 +1,6 @@
+# Use list comprehension to create a list of tuples
+# of first and last letter of the word
+
+input = ['Apricot', 'Cat', 'Dog', 'Ocelot', 'Zebra', 'Bat', 'Orange']
+output = [(word[0], word[len(word)-1]) for word in input]
+print(output)
