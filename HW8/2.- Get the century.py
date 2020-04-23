3e0809bb55b3cbaccf7df9cede34b367658728e8
Larisa <larisaplantation@gmail.com>
2020-04-23 12:09:12 -0400

    Get the century

--- HW8/2.- Get the century.py
+++ HW8/2.- Get the century.py
@@ -2,5 +2,17 @@
 #Create a function that takes in a year and returns the correct century.
 def getCentury(year):
     import math
-    return math.ceil(year/100)
+    cent = math.ceil(year/100)
+    if cent == 1 or cent == 21:
+        letters = 'st'
+    elif cent == 2 or cent == 22:
+        letters = 'nd'
+    else:
+        letters = 'th'
+    return f"{math.ceil(year/100)}{letters} century"
 print(getCentury(1756))
+print(getCentury(1555))
+print(getCentury(1000))
+print(getCentury(1001))
+print(getCentury(2005))
+print(getCentury(2103))
