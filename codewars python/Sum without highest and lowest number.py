39a01354369b999154b54617c8217094cdaae69a
Larisa <larisaplantation@gmail.com>
2020-04-24 21:04:05 -0400

    Sum without highest and lowest number

--- codewars python/Sum without highest and lowest number.py
+++ codewars python/Sum without highest and lowest number.py
@@ -1,9 +1,4 @@
 def sum_array(arr):
     if not arr or len(arr) <= 1:
         return 0
-    sum = 0
-    for el in arr:
-        sum += el
-    max1 = max(arr)
-    min1 = min(arr)
-    return sum - max1 - min1
+    return sum(arr) - max(arr) - min(arr)
