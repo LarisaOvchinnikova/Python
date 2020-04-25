8f5f52f14bf8cb6e6e0f3cc7478cc9a52a840c67
Larisa <larisaplantation@gmail.com>
2020-04-25 15:48:35 -0400

    get century

--- HW8/2.- Get the century.py
+++ HW8/2.- Get the century.py
@@ -3,18 +3,21 @@
 def getCentury(year):
     import math
     cent = math.ceil(year/100)
-    if cent == 1 or cent == 21:
+    cent = str(cent)
+    print(cent[-1])
+    if cent[-1] == "1" and cent != '11':
         letters = 'st'
-    elif cent == 2 or cent == 22:
+    elif cent[-1] == "2" and cent != '12':
         letters = 'nd'
-    elif cent == 3 or cent == 23:
+    elif cent[-1] == "3" and cent != '13':
         letters = "rd"
     else:
         letters = 'th'
-    return f"{math.ceil(year/100)}{letters} century"
+    return f"{cent}{letters} century"
 print(getCentury(1756))
 print(getCentury(1555))
 print(getCentury(1000))
 print(getCentury(1001))
 print(getCentury(2005))
 print(getCentury(2103))
+print(getCentury(2299))
