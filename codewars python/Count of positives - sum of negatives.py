6327ac954492423ac3d48e24a5048835feaf2a82
Larisa <larisaplantation@gmail.com>
2020-04-24 20:38:51 -0400

    Count positive sum negative

---
+++ codewars python/Count of positives - sum of negatives.py
@@ -0,0 +1,11 @@
+def count_positives_sum_negatives(arr):
+    if len(arr) == 0:
+        return []
+    sum = 0
+    count = 0
+    for el in arr:
+        if el < 0:
+            sum += el
+        elif el > 0:
+            count += 1
+    return [count, sum]
