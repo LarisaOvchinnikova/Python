5b4c7b7cf9a6b07749ae761305d88068c4a233ba
Larisa <larisaplantation@gmail.com>
2020-04-26 10:18:46 -0400

    get century

--- HW8/3а - Date Format.py
+++ HW8/3а - Date Format.py
@@ -5,7 +5,8 @@
 def format_date(date, month, year):
     month_list = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November",
                   "December"]
-    return f"{month_list[month - 1]} {date}, {year}"
+    month = month_list[month - 1]
+    return f"{month} {date}, {year}"
 
 
 print(format_date(5, 5, 1789))
