04e0de33dd8eb100a934eaa245b7bfb14eed4067
Larisa <larisaplantation@gmail.com>
2020-04-26 21:06:26 -0400

    longest sequence of zeros1

---
+++ HW8/6b -  Longest Sequence of Consecutive Zeroes.py
@@ -0,0 +1,10 @@
+#⭐Longest Sequence of Consecutive Zeroes
+
+def longest_zero(s):
+    x = s.split("1")
+    return max(x)  # alphabet max,  0000>0
+
+
+print(longest_zero("01100001011000"))
+print(longest_zero("100100100"))
+print(longest_zero("1111"))
