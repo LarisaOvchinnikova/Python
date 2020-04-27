d55bfb837d4b3cc15692d7f038ea15d838982e1e
Larisa <larisaplantation@gmail.com>
2020-04-26 20:51:11 -0400

    sum fractions without list

---
+++ HW8/5a - Sum Fractions.py
@@ -0,0 +1,7 @@
+def sum_fractions(arr):
+    return round(sum([el[0] / el[1] for el in arr]))
+
+
+print(sum_fractions([[18, 13], [4, 5]]))
+print(sum_fractions([[36, 4], [22, 60]]))
+print(sum_fractions([[11, 2], [3, 4], [5, 4], [21, 11], [12, 6]]))
