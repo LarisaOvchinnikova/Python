cb61ab8e0f85c64eb163059f2ae890085de97019
Larisa <larisaplantation@gmail.com>
2020-04-27 22:52:20 -0400

    sorted

--- HW9 - sets. files/1.2 -  Sort unique users.py
+++ HW9 - sets. files/1.2 -  Sort unique users.py
@@ -1,7 +1,5 @@
 users = ['user62', 'user37', 'user34', 'user79', 'user42', 'user76', 'user39', 'user37', 'user76', 'user68', 'user34', 'user27', 'user7', 'user93', 'user26']
 
 x = set(users)
-print(x)
-
 sorted_users = sorted(list(x))
 print(sorted_users)
---
+++ HW9 - sets. files/1.2a -  Sort unique users.py
@@ -0,0 +1,9 @@
+
+def sor_ted(users):
+    users = set(users)
+    users = list(users)
+    sorted_users = sorted(users)
+    return sorted_users
+
+users = ['user62', 'user37', 'user34', 'user79', 'user42', 'user76', 'user39', 'user37', 'user76', 'user68', 'user34', 'user27', 'user7', 'user93', 'user26']
+print(sor_ted(users))
