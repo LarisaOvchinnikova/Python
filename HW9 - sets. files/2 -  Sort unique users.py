2ff7d5a863c77cb3780ab1e91532fdb856610962
Larisa <larisaplantation@gmail.com>
2020-04-27 20:39:20 -0400

    Sort users in array

---
+++ HW9 - sets. files/2 -  Sort unique users.py
@@ -0,0 +1,7 @@
+users = ['user62', 'user37', 'user34', 'user79', 'user42', 'user76', 'user39', 'user37', 'user76', 'user68', 'user34', 'user27', 'user7', 'user93', 'user26']
+
+x = set(users)
+print(x)
+
+sorted_users = list(x)
+print(sorted_users)
