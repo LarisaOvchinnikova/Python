34b53edb66fd307219092811acd7a059b06366ae
Larisa <larisaplantation@gmail.com>
2020-04-27 22:53:43 -0400

    sorted

--- HW9 - sets. files/2.1 - Intersection.py
+++ HW9 - sets. files/2.1 - Intersection.py
@@ -3,4 +3,4 @@ users_yesterday = set(yesterday_users)
 today_users = ['user2', 'user39', 'user41', 'user38', 'user50', 'user48', 'user22', 'user44', 'user6', 'user9']
 users_today = set(today_users)
 yesterday_and_today = users_yesterday & users_today
-print(yesterday_and_today)
+print(list(yesterday_and_today))
