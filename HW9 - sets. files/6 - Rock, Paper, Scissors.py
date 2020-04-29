686a9222cd0a11ed214c18873a6979d48ae27e37
Larisa <larisaplantation@gmail.com>
2020-04-28 22:47:41 -0400

    Rock, Paper, Scissors

---
+++ HW9 - sets. files/6 - Rock, Paper, Scissors.py
@@ -0,0 +1,19 @@
+# Rock beats Scissors
+# Scissors beats Paper
+# Paper beats Rock
+def rps(player1, player2):
+    player1 = player1.lower();
+    player2 = player2.lower()
+    rules = {
+        "rock" : "scissors",
+        "scissors": "paper",
+        "paper": "rock"
+    }
+    print(rules["rock"] )
+    if rules[player1] == player2:
+        return "The winner is player1"
+    elif player1 == player2:
+        return "It is draw"
+    else:
+        return "The winner is player2"
+print(rps("Rock", "Paper"))
