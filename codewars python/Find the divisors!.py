5c9b98df4b14c8562275cc72b0b4d411d5305a66
Larisa <larisaplantation@gmail.com>
2020-05-01 00:30:35 -0400

    added codewars solution

---
+++ codewars python/Find the divisors!.py
@@ -0,0 +1,9 @@
+def divisors(n):
+    res = []
+    for i in range(2, n // 2 + 1):
+        if n % i == 0:
+            res.append(i)
+    if len(res) == 0:
+        return f"{n} is prime"
+    else:
+        return res
