5ad6c6e7212fe6e52b988cd98b70c3e22d8d16bc
Larisa <larisaplantation@gmail.com>
2020-05-02 13:01:23 -0400

    added codewars solution

---
+++ codewars python/Find the odd int.py
@@ -0,0 +1,10 @@
+# Given an array, find the integer that appears an odd number of times.
+
+def find_it(seq):
+    c = [el for el in seq if seq.count(el) % 2 != 0]
+    arr = list(set(c))
+    return arr[0]
+
+# --- 2 case
+def find_it(seq):
+    return [el for el in seq if seq.count(el) % 2 != 0][0]
