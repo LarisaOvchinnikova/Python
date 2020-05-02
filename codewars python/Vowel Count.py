d6971e5bca4dc1efecc638847e6ff50a1573c5ab
Larisa <larisaplantation@gmail.com>
2020-05-02 12:08:58 -0400

    added codewars solution

--- codewars python/Vowel Count.py
+++ codewars python/Vowel Count.py
@@ -11,3 +11,8 @@ def getCount(inputStr):
 def getCount(inputStr):
     num_vowels = [1 for letter in inputStr if letter in "aeuio"].count(1)
     return num_vowels
+
+# -----3 case
+def getCount(inputStr):
+    num_vowels = [1 for letter in inputStr if letter in "aeuio"]
+    return len(num_vowels)
