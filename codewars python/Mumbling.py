f1a72aea88da704f0336a6a24b9f1a536ff81431
Larisa <larisaplantation@gmail.com>
2020-05-01 21:55:14 -0400

    added codewars solution

--- codewars python/Mumbling.py
+++ codewars python/Mumbling.py
@@ -3,3 +3,7 @@ def accum(s):
     for i in range(len(s)):
         s1 = s1 + s[i].upper() + s[i].lower() * i + "-"
     return s1[:-1]
+
+# === 2 case
+def accum(s):
+    return '-'.join([c.upper() + c.lower() * i for i, c in enumerate(s)])
