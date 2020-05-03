09c46b717ea8f7f332b38e6a7b4fa486aef1da4f
Larisa <larisaplantation@gmail.com>
2020-05-03 15:09:02 -0400

    added codewars solution

--- codewars python/Total amount of points.py
+++ codewars python/Total amount of points.py
@@ -6,3 +6,7 @@ def points(games):
         elif int(el[0]) == int(el[2]):
             s += 1
     return s
+# ----2 case
+def points(games):
+    s = sum([3 if el[0]>el[2] else 0 if el[0]<el[2] else 1 for el in games])
+    return s
