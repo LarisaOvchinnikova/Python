fe4cbca37781c8f21fb4de698e9e46fb8870b3eb
Larisa <larisaplantation@gmail.com>
2020-05-04 12:48:23 -0400

    riddle

---
+++ tasks for students/riddles.py
@@ -0,0 +1,5 @@
+answer = input("What orange vegetables souds like a parrot? ")
+if answer == "carrot" or answer == "a carrot":
+    print("Yes! You guessed it!")
+else:
+    print("You'll be lucky next time")
