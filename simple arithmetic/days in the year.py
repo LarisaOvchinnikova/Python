bdfa1bd79ac9e359132a762a496dc531b5e62e05
Larisa <larisaplantation@gmail.com>
2020-05-05 12:29:38 -0400

    days in the year

--- simple arithmetic/days in the year.py
+++ simple arithmetic/days in the year.py
@@ -2,10 +2,10 @@ year = int(input("Enter year: "))
 
 if year % 4 == 0:
     if year % 100 == 0 and year % 400 != 0:
-        year = 365
+        days = 365
     else:
-        year = 366
+        days = 366
 else:
-    year = 365
+    days = 365
 
-print("Количество дней в году:", year)
+print("Количество дней в году:", days)
