b39518ba12491a5796cc0983cfb855dfe3c294c5
Larisa <larisaplantation@gmail.com>
2020-05-05 00:32:56 -0400

    add codewars solution

--- codewars python/Unique In Order.py
+++ codewars python/Unique In Order.py
@@ -5,3 +5,7 @@ def unique_in_order(s):
        if char != s[i-1] or i == 0:
            res.append(s[i])
     return res
+
+# --- 2 case
+def unique_in_order(s):
+    return [char for i, char in enumerate(s) if char != s[i-1] or i == 0]
