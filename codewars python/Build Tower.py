79301e1e3beb4569a3cda365d3d8717de7f0dc5f
Larisa <larisaplantation@gmail.com>
2020-05-06 18:51:32 -0400

    add codewars solution

--- codewars python/Build Tower.py
+++ codewars python/Build Tower.py
@@ -8,3 +8,7 @@ def tower_builder(n_floors):
             i += 2
             count_floors += 1
         return lst
+
+# 2  case
+def tower_builder(n):
+    return [("*" * (i*2-1)).center(n*2-1) for i in range(1, n+1)]
