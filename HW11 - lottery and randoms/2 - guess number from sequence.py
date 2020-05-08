b4ab80fdeda547c74accb406dc744ccc46c40890
Larisa <larisaplantation@gmail.com>
2020-05-08 12:18:40 -0400

    guess number from sequence

--- HW11 - lottery and randoms/2 - guess number from sequence.py
+++ HW11 - lottery and randoms/2 - guess number from sequence.py
@@ -1,17 +1,19 @@
 import random
-sequence = []
-length = random.randint(1, 15)
-coefficient = random.randint(1, 20)
-for i in range(length):
-    sequence.append(i*coefficient)
-# print(sequence)
-x = random.randint(0, length - 1)
-missed_number = sequence[x]
-sequence[x] = "..."
-print("Guess missed number: ")
-print(sequence)
-answer = int(input("Enter your answer: "))
-if answer == missed_number:
-    print("Good job!")
-else:
-    print("Try next time")
+attempts = int(input("How many times you want to play? "))
+for i in range(attempts):
+    sequence = []
+    length = random.randint(3, 15)
+    coefficient = random.randint(1, 20)
+    for i in range(length):
+        sequence.append(i*coefficient)
+
+    x = random.randint(0, length - 1)
+    missed_number = sequence[x]
+    sequence[x] = "..."
+    print("Guess missed number: ")
+    print(sequence)
+    answer = int(input("Enter your answer: "))
+    if answer == missed_number:
+        print("Good job!")
+    else:
+        print("Try next time")
