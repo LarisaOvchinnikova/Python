602dd99014e5990f2dbd704f20e2a6ab36d91819
Larisa <larisaplantation@gmail.com>
2020-05-10 00:37:25 -0400

    guess number improve

--- tasks for students/guess number.py
+++ tasks for students/guess number.py
@@ -1,17 +1,21 @@
-import random
+# import random
+from random import randint
 from datetime import datetime, date, timedelta
 
-num = random.randint(0,100) # random integer [0-10]
+#num = random.randint(0,100) # random integer [0-10]
+num = randint(1, 100)
 print("I know the number from 0 to 100. What number I have?")
 answer = int(input("Guess the number: "))
-start = datetime.today() # в начале программы
+start = datetime.today()
+count = 1
 while not answer == num:
     if answer < num:
-        print("greater")
+        print("Too small")
     elif answer > num:
-        print("less")
+        print("Too big")
     answer = int(input("Guess the number: "))
-end = datetime.today() #  в конце программы
+    count += 1
+end = datetime.today()
 print(f"You got it! It is {num}")
 diff = (end - start).seconds
-print(f"It took {diff} seconds")
+print(f"It took {diff} seconds. I had {count} attempts")
