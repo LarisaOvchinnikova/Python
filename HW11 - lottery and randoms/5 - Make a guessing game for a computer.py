313ecdbaa4f16e925348844a8ae181f67ffbacaf
Larisa <larisaplantation@gmail.com>
2020-05-10 23:46:13 -0400

    computer guess number

---
+++ HW11 - lottery and randoms/5 - Make a guessing game for a computer.py
@@ -0,0 +1,21 @@
+# Пользователь загадывает число через инпут,
+# а компьютер рандомно пытается угадать, с помощью тех же подсказок
+# "Too big" или "Too small". Посчитайте количество попыток и время игры.
+from random import randint
+
+secret_number = int(input("Enter number in range 1-100 and computer will guess it: "))
+
+low = 1
+high = 100
+count =  1
+computer_guess = randint(low, high)
+while computer_guess != secret_number:
+    print(f"Computer think that it is {computer_guess}")
+    result = int(input("Too big (1) or too small(2)? "))
+    if result == 1:
+        high = computer_guess - 1
+    if result == 2:
+        low = computer_guess + 1
+    computer_guess = randint(low, high)
+    count += 1
+print(f"Your number is {computer_guess}. Took {count} guesses.")
