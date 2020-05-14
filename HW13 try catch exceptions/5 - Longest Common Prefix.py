b5bc5c12563de86bd70ac10eb8c7affa3da56c6b
Larisa <larisaplantation@gmail.com>
2020-05-13 20:39:41 -0400

    Longest Common Prefix

--- HW13 try catch exceptions/5 - Longest Common Prefix.py
+++ HW13 try catch exceptions/5 - Longest Common Prefix.py
@@ -1,3 +1,5 @@
+from random import randint, choice
+
 def common_prefix(arr):
     start = 0
     end = 1
@@ -13,6 +15,21 @@ def common_prefix(arr):
         common_pr = prefix
         end += 1
     return common_pr
-print(common_prefix(["flower","flow","flight"]))
-print(common_prefix(["dog","racecar","car"]))
+#print(common_prefix(["flower","flow","flight"]))
+#print(common_prefix(["dog","racecar","car"]))
 print(common_prefix(['monkey', 'monday']))
+
+
+f = open("most_common_words.txt")
+words = f.read().split()
+f.close()
+words.sort()
+
+lst = []
+x = randint(1,500)
+# while len(lst) < size:
+#     lst.append(choice(words))
+for i in range(10):
+    lst.append(choice(words[x:x+10]))
+print(lst)
+print(common_prefix(lst))
