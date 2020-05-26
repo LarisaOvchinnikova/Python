2b243a3fa41ae27482751ba184ae8068f1cc8fe9
Larisa <larisaplantation@gmail.com>
2020-05-25 22:52:38 -0400

    add codewars solution

---
+++ codewars python/Convert string to camel case.py
@@ -0,0 +1,6 @@
+def to_camel_case(text):
+    if text == "":
+        return ""
+    new_text = text.replace('-', ' ').replace('_', ' ').split(" ")
+
+    return "".join([el.title() if i > 0 else el for i, el in enumerate(new_text)])
