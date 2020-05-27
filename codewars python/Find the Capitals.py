055b66504e6128266da34e67febb5022be07bcba
Larisa <larisaplantation@gmail.com>
2020-05-26 23:17:29 -0400

    codewars solution

---
+++ codewars python/Find the Capitals.py
@@ -0,0 +1,8 @@
+def capital(capitals):
+    res = []
+    for el in capitals:
+        if 'state' in el:
+            res.append(f"The capital of {el['state']} is {el['capital']}")
+        else:
+            res.append(f"The capital of {el['country']} is {el['capital']}")
+    return res
