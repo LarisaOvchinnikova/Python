122954e833db79d32f2aa8eed9dbfea7d0a227ea
Larisa <larisaplantation@gmail.com>
2020-06-04 19:30:22 -0400

    HW2-2

--- 1 - Python-2/2 - if else, true-false/HW2/2 - number of letters.py
+++ 1 - Python-2/2 - if else, true-false/HW2/2 - number of letters.py
@@ -1,2 +1,5 @@
 name = input("What's your name? ")
-print(f"Hello, {name}! Your name has {len(name)} letters! ")
+if len(name) > 6:
+    print(f"Wow! {name}, your name has lot of letters: {len(name)}")
+else:
+    print(f"Hello, {name}! Your name has only {len(name)} letters! ")
