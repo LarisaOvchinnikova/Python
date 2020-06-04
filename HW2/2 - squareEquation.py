937f1dfd96e6c34963553c650df91de6530f1263
Larisa <larisaplantation@gmail.com>
2020-06-04 19:48:14 -0400

    HW2-22

---
+++ HW2/2 - squareEquation.py
@@ -0,0 +1,17 @@
+a = int(input('Input number a: '))
+b = int(input('Input number b: '))
+c = int(input('Input number c: '))
+print(f"{a}x^2 + {b}x - {c} = 0")
+if a == 0:
+  print("Error")
+else:
+  d = b ** 2 - 4 * a * c
+  if d > 0:
+    x1 = (-b + d ** 0.5) / (2 * a)
+    x2 = (-b - d ** 0.5) / (2 * a)
+    print('x1 = ', round(x1, 2), 'x2 = ', round(x2, 2))
+  elif d == 0:
+    x = -b / ( 2* a)
+    print('x = ', round(x, 2))
+  else:
+    print('No roots')
