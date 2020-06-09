e41f0d51b0341a37f3fa8357ef125ebebc3b7eb3
Larisa <larisaplantation@gmail.com>
2020-06-08 23:16:28 -0400

    properties of string

---
+++ 1 - Python-2/4 - strings functions/Determine the properties of a string.py
@@ -0,0 +1,33 @@
+#Print word " has lowercase letters" if it has only lowercase alphabet characters
+# Print word " has uppercase letters"
+# if it has only uppercase alphabet characters
+# Print word " has so many letters. Much wow."
+# if it has both uppercase and lowercase alphabet characters but no digits
+# Print word " has digits" if
+# it has only digits
+# Print word " has digits and lowercase letters"
+# if it has digits and only lowercase letters
+# Print word " has digits and uppercase letters"
+# if it has digits and only uppercase letters
+# Print word " has all kinds of stuff"
+# if it has digits and both uppercase and lowercase letters
+
+word = input("Enter word: ")
+if word.isdigit():
+    print("Has digits")
+elif word.isalpha():
+    if word.islower():
+        print("Has lowercase")
+    elif word.isupper():
+        print("Has uppercase")
+    else:
+        print("Has upper and lower")
+elif word.isalnum():
+    if word.islower():
+        print("Has lowercase and digits")
+    elif word.isupper():
+        print("Has uppercase and digits")
+    else:
+        print("Has upper and lower and digits")
+
+        
---
+++ 1 - Python-2/4 - strings functions/lesson 4.py
@@ -0,0 +1,7 @@
+print("123".isdigit())   #True
+print("123.0".isdigit()) #False
+print("123a0".isalnum())  #True
+print("asd".isalpha())  #True
+print("hello".center(10))
+print("HELLO".isupper()) #True
+print("123hello".islower()) #True
