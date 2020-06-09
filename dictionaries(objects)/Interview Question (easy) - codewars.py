d0b8185adf338dac44767bbbe9f10568e9202042
Larisa <larisaplantation@gmail.com>
2020-06-08 22:44:35 -0400

    codewars solution

--- dictionaries(objects)/Interview Question (easy) - codewars.py
+++ dictionaries(objects)/Interview Question (easy) - codewars.py
@@ -1,14 +1,16 @@
 def get_strings(city):
     obj = {}
     city = list(city.lower())
-    print(city)
+    s = ''
     for el in city:
         if not el in obj:
             obj[el] = "*"
         else:
             obj[el] += "*"
 
-    return list(obj)
-
+    for keys  in obj:
+        print(keys, obj[keys])
+        s = s + f"{keys}:{obj[keys]},"
+    return s[:-1]
 
 print(get_strings("Chicago"))
