dc2e6d5663e9f6949fcf71762efcb52cf2caecbb
Larisa <larisaplantation@gmail.com>
2020-06-14 13:18:46 -0400

    list comprehension-1

---
+++ 1 - Python-2/6 - List Comprehensions/HW6/a filtered list of elements which start with an uppercase letter..py
@@ -0,0 +1,5 @@
+lst = ['Apricot', 'cat', 'Dog', 'ocelot', 'Zebra', 'bat', 'orange']
+filtered_list = [el for el in lst if el[0].isupper()]
+print(filtered_list)
+#filtered_list = ['Apricot', 'Dog', 'Zebra']
+
