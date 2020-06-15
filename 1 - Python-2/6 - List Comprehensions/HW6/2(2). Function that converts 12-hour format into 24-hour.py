53c5533ad30dc8741ce1bce4de91cc7ae2246e54
Larisa <larisaplantation@gmail.com>
2020-06-14 22:16:02 -0400

    list comprehension-7

---
+++ 1 - Python-2/6 - List Comprehensions/HW6/2(2). Function that converts 12-hour format into 24-hour.py
@@ -0,0 +1,21 @@
+def get_military_time(us_time):
+    ampm = us_time[-2:]
+    minutes = us_time[3:5]
+    hour = int(us_time[0:2])
+    if ampm == "PM":
+        if hour != 12:
+            hour = hour + 12
+        elif hour == 12:
+            hour = 0
+    hour = str(hour)
+    if len(hour) < 2:
+        hour = "0" + hour
+    milTime = hour + ":" + minutes
+    return milTime
+
+
+print((get_military_time("07:05 PM")))
+print((get_military_time("07:05 AM")))
+print((get_military_time("12:05 AM")))
+print((get_military_time("12:05 PM")))
+
