78b5d63eff2480678f2369bc85e11f63c41da522
Larisa <larisaplantation@gmail.com>
2020-06-22 22:55:28 -0400

    one more solution

--- 1 - Python-2/7 - Tuples/HW7/1(2) - Sum Fractions.py
+++ 1 - Python-2/7 - Tuples/HW7/1(2) - Sum Fractions.py
@@ -2,6 +2,14 @@ def sum_fractions(arr):
     return round(sum([el[0] / el[1] for el in arr]))
 
 
+def sum_fr(arr):
+    return round(sum([numerator/denominator for numerator, denominator in arr]))
+
+
+
+
 print(sum_fractions([[18, 13], [4, 5]]))
-print(sum_fractions([[36, 4], [22, 60]]))
-print(sum_fractions([[11, 2], [3, 4], [5, 4], [21, 11], [12, 6]]))
+# print(sum_fractions([[36, 4], [22, 60]]))
+# print(sum_fractions([[11, 2], [3, 4], [5, 4], [21, 11], [12, 6]]))
+
+print(sum_fr([[18,13], [4,5]]))
