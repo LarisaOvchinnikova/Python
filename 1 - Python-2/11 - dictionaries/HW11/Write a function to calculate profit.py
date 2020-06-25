d87852bf3c33da363c67c3ca6c80e2dd47821cb8
Larisa <larisaplantation@gmail.com>
2020-06-25 15:25:39 -0400

    HW11

---
+++ 1 - Python-2/11 - dictionaries/HW11/Write a function to calculate profit.py
@@ -0,0 +1,18 @@
+def profit(dict):
+    return round(dict["inventory"] * (dict["sell_price"] - dict["cost_price"]))
+
+print(profit({
+  "cost_price": 32.67,
+  "sell_price": 45.00,
+  "inventory": 1200
+}))
+print(profit({
+  "cost_price": 225.89,
+  "sell_price": 550.00,
+  "inventory": 100
+}))
+print(profit({
+  "cost_price": 2.77,
+  "sell_price": 7.95,
+  "inventory": 8500
+}))
