a44cc8976ba44f19636dddbcba171b723cc01732
Larisa <larisaplantation@gmail.com>
2020-06-26 16:28:23 -0400

    HW11

---
+++ 1 - Python-2/11 - dictionaries/HW11/Tests results.py
@@ -0,0 +1,19 @@
+def test(r):
+    average = round(sum(r) / len(r), 3)
+    dct = {
+        "h": 0,
+        "a": 0,
+        "l": 0
+    }
+    for el in r:
+        if el >= 9:
+            dct["h"] += 1
+        elif el >= 7:
+            dct["a"] += 1
+        else:
+            dct["l"] += 1
+
+    if dct["a"] + dct["l"] == 0:
+        return [average, dct, "They did well"]
+    else:
+        return [average, dct]
