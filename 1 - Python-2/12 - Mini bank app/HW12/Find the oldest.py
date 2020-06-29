ac34ddd66e606a790e9ce133d580659f427a6965
Larisa <larisaplantation@gmail.com>
2020-06-28 23:02:28 -0400

    HW12

--- 1 - Python-2/12 - Mini bank app/HW12/Find the oldest.py
+++ 1 - Python-2/12 - Mini bank app/HW12/Find the oldest.py
@@ -1,14 +1,20 @@
 def oldest(dct):
-    arr = max(dct.values())
-    print(arr)
+    m = max(dct.values())
+    for key, value in dct.items():
+        if value == m:
+            return key
 
-
-    #return
 
 print(oldest({
   "Emma": 71,
   "Jack": 45,
   "Amy": 15,
   "Ben": 29
-})
-)
+}))
+
+print(oldest({
+  "Max": 9,
+  "Josh": 13,
+  "Sam": 48,
+  "Anne": 33
+}))
