60ee7555b4560029930295f4712ac55b3b1b3241
Larisa <larisaplantation@gmail.com>
2020-07-01 01:25:44 -0400

    HW13

--- 1 - Python-2/13 - files/HW13/1. Read the book and count words.py
+++ 1 - Python-2/13 - files/HW13/1. Read the book and count words.py
@@ -1,27 +1,22 @@
 import string
 x = open("harry potter and the sorcerer's stone.txt", encoding="utf-8-sig")
-text = x.read()  # все одной строкой через \n
+text = x.read()
 text_new = ""
 for char in text:
     if not char in string.punctuation:
         text_new += char
 text = text_new.lower().split()
-print(text)
+
 y = open("stopwords.txt", encoding="utf-8-sig")
 stop_words = y.read()
 stop_words = stop_words.lower().split()
-print(stop_words)
-
 
 dct = {text.count(word): word for word in text if not word in stop_words}
-print(dct)
 arr = list(dct.items())
-print('----')
-print(arr)
 arr.sort(reverse=True)
-dct = {word[1]: arr.count(word[1]) for word in arr}
-print(dict(list(dct)[:10]))
+
+dct = {word[1]: word[0] for word in arr[:20]}
+print(dct)
 x.close()
 y.close()
 
-#sorted(dct.values())
