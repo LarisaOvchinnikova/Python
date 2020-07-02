dcfea7efd1eb39dbfd9705d4604ea10dd111be30
Larisa <larisaplantation@gmail.com>
2020-07-02 14:56:33 -0400

    codewars solition

--- codewars python/if else/Students Final Grade.py
+++ codewars python/if else/Students Final Grade.py
@@ -4,7 +4,7 @@ def final_grade(exam, projects):
         return 100
     elif exam > 75 and projects >= 5:
         return 90
-    elif exam > 50 and projects >=2:
+    elif exam > 50 and projects >= 2:
         return 75
     else:
         return 0
