531c5bc6bf40bd9324df2573d8a36c35f65933ac
Larisa <larisaplantation@gmail.com>
2020-07-01 20:12:07 -0400

    HW13 baby names ranking

--- 1 - Python-2/13 - files/HW13/2. Baby Names Ranking.py
+++ 1 - Python-2/13 - files/HW13/2. Baby Names Ranking.py
@@ -1,21 +1,22 @@
 year = input("Enter the year: ")
 name = input("Enter the name: ")
 x = open(f"babynamesranking{year}.txt")
-t = x.readlines()
-
-if not name in t:
-    print(f"The name {name} is not ranked in year {year}")
-else:
-
-    names = [el.split() for el in text if name in el]
-    print(text)
-    print(names)
-    print(names[0])
-    i = names[0].index(name)
-    print(i)
-    print(f"{name} is ranked #{names[0][0]} in year {year}")
-    if i == 1:
-        gender = "boys"
+text = x.readlines()
+fl = False
+for string in text:
+    if name not in string:
+        continue
     else:
-        gender = "girls"
-    print(f"{names[0][i+1]} {gender} named {name}!")
+        fl = True
+        names = string.split()
+        i = names.index(name)
+        print(f"{name} is ranked #{names[0]} in year {year}")
+        if i == 1:
+            gender = "boys"
+        else:
+            gender = "girls"
+        print(f"{names[i+1]} {gender} named {name}!")
+        break
+
+if not fl:
+    print(f"The name {name} is not ranked in year {year}")
