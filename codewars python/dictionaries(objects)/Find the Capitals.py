9455b632f2de211b233dd35fabecff0c603f6dca
Larisa <larisaplantation@gmail.com>
2020-07-03 18:07:07 -0400

    HW14

--- codewars python/dictionaries(objects)/Find the Capitals.py
+++ codewars python/dictionaries(objects)/Find the Capitals.py
@@ -1,3 +1,4 @@
+https://www.codewars.com/kata/53573877d5493b4d6e00050c/train/python
 def capital(capitals):
     res = []
     for el in capitals:
@@ -7,3 +8,6 @@ def capital(capitals):
             res.append(f"The capital of {el['country']} is {el['capital']}")
     return res
 print()
+
+ef capital(capitals):
+    return [f"The capital of {el['state']} is {el['capital']}" if "state" in el else f"The capital of {el['country']} is {el['capital']}"  for el in capitals]
