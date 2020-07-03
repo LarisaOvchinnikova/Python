ebf6a33e6a1f408c7b58df96e89eb35333b322aa
Larisa <larisaplantation@gmail.com>
2020-07-03 09:25:21 -0400

    HW14

---
+++ 1 - Python-2/14 - files, collections/HW14/Average Word Length.py
@@ -0,0 +1,14 @@
+def average_word_length(arr):
+    lst = [len(word) for word in arr]
+    s = sum(lst) / len(lst)
+    return round(s, 2)
+
+
+# lst = ["banana", "apple", "grape", "mom", "a"]
+
+
+f = open("most_common_words.txt")
+lst = f.read().split()
+f.close()
+
+print(average_word_length(lst))
