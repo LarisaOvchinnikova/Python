b392b0463f659be30a9ed46c79edefd99d3da397
Larisa <larisaplantation@gmail.com>
2020-07-03 14:03:43 -0400

    codewars

--- codewars python/loops/Deodorant Evaporator.py
+++ codewars python/loops/Deodorant Evaporator.py
@@ -1,8 +1,8 @@
 https://www.codewars.com/kata/523b4ff7adca849afe000035/train/python
 
 def evaporator(content, evap_per_day, threshold):
-    n = 0;
-    limit = content * threshold/100;
+    n = 0
+    limit = content * threshold/100
     while content > limit:
         content = content - content * evap_per_day/100
         n += 1
