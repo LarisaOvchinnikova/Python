3af22dc28ccef6f0437103c6097c38efeeac9768
Larisa <larisaplantation@gmail.com>
2020-07-06 11:39:53 -0400

    codewars

--- codewars python/loops/Beginner Series 3 Sum of Numbers.py
+++ codewars python/loops/Beginner Series 3 Sum of Numbers.py
@@ -9,5 +9,9 @@ def get_sum(a,b):
    return s
 
 
+def get_sum(a, b):
+   return sum([i for i in range(min(a, b), max(a, b) + 1)])
+
+
 print(get_sum(1,2))
 print(get_sum(1,0))
--- codewars python/loops/Build Tower.py
+++ codewars python/loops/Build Tower.py
@@ -13,3 +13,7 @@ def tower_builder(n_floors):
 # 2  case
 def tower_builder(n):
     return [("*" * (i*2-1)).center(n*2-1) for i in range(1, n+1)]
+
+# 3 case
+def tower_builder(n):
+    return [("*" * i).center(n * 2 - 1) for i in range(1, n * 2, 2)]
