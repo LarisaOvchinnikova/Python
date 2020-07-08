284d88892eae5a1f5428a0e9236316b223e03141
Larisa <larisaplantation@gmail.com>
2020-07-07 20:16:38 -0400

    codewars

--- 1 - Python-2/16 - Time/HW16/2a) Where my anagrams at.py
+++
@@ -1,9 +0,0 @@
-def anagrams(word, words):
-    match = {}
-    for char in word:
-        match[char] = word.count(char)
-    dct = {'a': 2, 'b': 2}
-    dct1  = {'b': 2, 'a': 2}
-    return dct1 == dct
-
-print(anagrams('abba', ['aabb', 'abcd', 'bbaa', 'dada']))
--- codewars python/linear/Expressions Matter.py
+++ codewars python/linear/Expressions Matter.py
@@ -1,3 +1,5 @@
+https://www.codewars.com/kata/5ae62fcf252e66d44d00008e
+
 def expression_matter(a, b, c):
     d1 = a+b+c
     d2 = a*(b+c)
