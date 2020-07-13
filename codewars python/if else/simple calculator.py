6836e962058acef089844ddecd54237423df3925
Larisa <larisaplantation@gmail.com>
2020-07-13 17:05:44 -0400

    codewars

--- codewars python/if else/simple calculator.py
+++ codewars python/if else/simple calculator.py
@@ -1,5 +1,7 @@
 # https://www.codewars.com/kata/5810085c533d69f4980001cf/train/python
 def calculator(a,b,op):
+    if type(a) != int or type(b) != int:
+        return 'unknown value'
     if op == '+':
         return a + b
     elif op == '-':
