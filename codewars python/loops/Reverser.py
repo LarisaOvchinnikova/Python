9db3fb6b537717070d10407a1cf9c65b57f1df1b
Larisa <larisaplantation@gmail.com>
2020-07-14 10:40:42 -0400

    codewars

---
+++ codewars python/loops/Reverser.py
@@ -0,0 +1,8 @@
+# https://www.codewars.com/kata/58069e4cf3c13ef3a6000168/train/python
+def reverse(n):
+    r = 0
+    while n > 0:
+        last = n % 10
+        r = r * 10 + last
+        n = n//10
+    return r
