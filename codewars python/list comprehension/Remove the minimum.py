5379e7008582c874e930abbce30a6d214451056d
Larisa <larisaplantation@gmail.com>
2020-07-14 20:06:00 -0400

    codewars

--- codewars python/list comprehension/Remove the minimum.py
+++ codewars python/list comprehension/Remove the minimum.py
@@ -1,7 +1,28 @@
-https://www.codewars.com/kata/563cf89eb4747c5fb100001b/python
+#https://www.codewars.com/kata/563cf89eb4747c5fb100001b/python
+# def remove_smallest(numbers):
+#     if len(numbers) == 0:
+#         return []
+#     else:
+#         m = min(numbers)
+#         return [el for i, el in enumerate(numbers) if i != numbers.index(m)]
+#
+
+# def remove_smallest(numbers):
+#     if len(numbers) == 0:
+#         return []
+#     m = min(numbers)
+#     arr = numbers[:]
+#     arr.remove(m)
+#     return arr
+#
+# print(remove_smallest([1,2,3,4,5]))
+
 def remove_smallest(numbers):
-    if len(numbers) == 0:
-        return []
+    arr = numbers[:]
+    if arr:
+        arr.remove(min(numbers))
     else:
-        m = min(numbers)
-        return [el for i, el in enumerate(numbers) if i != numbers.index(m)]
+        return []
+    return arr
+
+print(remove_smallest([1,2,3,4,5]))
