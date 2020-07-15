870fad94fa767ed4558b2c40b84e95779b2f7a87
Larisa <larisaplantation@gmail.com>
2020-07-15 14:39:30 -0400

    datetime transform time

--- 1 - Python-2/19 - datetime/HW19/1. Transform a string of 12-hour format to a 24-hour format with datetime and vice versa.py
+++ 1 - Python-2/19 - datetime/HW19/1. Transform a string of 12-hour format to a 24-hour format with datetime and vice versa.py
@@ -5,5 +5,13 @@ def time_to_24h(time_12):
     time_24 = time.strftime("%H:%M")
     return time_24
 
+def time_to_12h(time_24):
+    time = datetime.strptime(time_24, "%H:%M")
+    time_12= time.strftime("%I:%M %p")
+    return time_12
+
 print(time_to_24h("1:30 PM"))
 print(time_to_24h("12:30 AM"))
+
+print(time_to_12h("14:30"))
+print(time_to_12h("11:30"))
