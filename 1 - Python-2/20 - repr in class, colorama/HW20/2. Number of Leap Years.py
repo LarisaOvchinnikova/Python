4a68658b38e312b3660b39b03a94c02add066011
Larisa <larisaplantation@gmail.com>
2020-07-17 00:51:43 -0400

    HW20

---
+++ 1 - Python-2/20 - repr in class, colorama/HW20/2. Number of Leap Years.py
@@ -0,0 +1,16 @@
+from datetime import date, timedelta
+
+def num_of_leapyears(s):
+    years = s.split("-")
+    count_leap_years = 0
+    year = int(years[0])
+    while year <= int(years[1]):
+        last_day_february = date(year, 3, 1) - timedelta(days=1)
+        if last_day_february.day == 29:
+            count_leap_years += 1
+        year += 1
+    return count_leap_years
+
+print(num_of_leapyears("1980-1984"))
+print(num_of_leapyears("2000-2020"))
+print(num_of_leapyears("1600-2000"))
