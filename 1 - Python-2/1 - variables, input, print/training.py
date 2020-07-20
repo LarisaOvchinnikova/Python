3436bc4f146c0b5cab43f333ba41fdebdc4f5583
Larisa <larisaplantation@gmail.com>
2020-07-20 18:42:04 -0400

    lesson 9

--- 1 - Python-2/1 - variables, input, print/training.py
+++
@@ -1,6 +0,0 @@
-word = "summer"
-print("*" * 8)
-print("*summer*")
-print("*" * 8)
-name = "John"
-print("Hello boss") if name == "Bill" else print("Hello guest")
---
+++ 1 - Python-2/3 - strings/training.py
@@ -0,0 +1,37 @@
+# word = "summer"
+# print("*" * 8)
+# print("*summer*")
+# print("*" * 8)
+# name = "John"
+# print("Hello boss") if name == "Bill" else print("Hello guest")
+#
+# print("a" in "apple")
+# word = "accessories"
+# i = word.find("a")
+# print(i)
+# print(word.find("s"))
+# print(word.find("b"))
+# print(word.find("s", 6)) # 10
+# print(word.find("s", 6, 9)) # -1
+
+# word = "september"
+# print(word.rfind("a"))
+# word = "september"
+# print(word.index("e")) # 1 (индекс первой буквы "e" в строке)
+# print(word.index("a")) # ValueError: substring not found
+
+word = "additional"
+print(word.rindex("a")) # 8 (индекс первой буквы "a" в строке)
+#print(word.rindex("b"))  # ValueError: substring not found
+
+text = "My eyes are blue"
+print(text.index("blue")) # 8 (индекс вхождения  строки "blue")
+
+word = "My flowers are nice"
+print(word.index("nice")) # 12 (индекс вхождения  строки "nice")
+
+text = "Be loyal to the one who is loyal to you."
+print(text.count("o"))
+
+text = "To be or not to be"
+print(text.count("be"))   # 2
