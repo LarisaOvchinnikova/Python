572d997df5bf8c94bdf195b32bebd327585cef58
Larisa <larisaplantation@gmail.com>
2020-07-21 17:36:06 -0400

    codewars solution

--- 1 - Python-2/20 - repr in class, colorama/HW20/4 - Calculator for lists.py
+++ 1 - Python-2/20 - repr in class, colorama/HW20/4 - Calculator for lists.py
@@ -18,27 +18,27 @@ class MathList:
         arr1, arr2 = add_zeros(arr1, arr2)
         for i in range(len(arr1)):
             res.append(arr1[i] + arr2[i])
-        return res
+        return MathList(res)
 
     def __sub__(self, other):
         arr1 = self.lst
         arr2 = other.lst
         arr1, arr2 = add_zeros(arr1, arr2)
-        return [arr1[i] - arr2[i] for i in range(len(arr1))]
+        return MathList([arr1[i] - arr2[i] for i in range(len(arr1))])
 
     def __mul__(self, other):
         arr1 = self.lst
         arr2 = other.lst
         arr1, arr2 = add_zeros(arr1, arr2)
-        return [arr1[i] * arr2[i] for i in range(len(arr1))]
+        return MathList([arr1[i] * arr2[i] for i in range(len(arr1))])
 
     def __floordiv__(self, other):
         arr1 = self.lst
         arr2 = other.lst
         arr1, arr2 = add_zeros(arr1, arr2)
 
-        return [arr1[i] // arr2[i] if arr2[i] != 0 else 0 for i in range(len(arr1))]
-        #return MathList(result)
+        return MathList([arr1[i] // arr2[i] if arr2[i] != 0 else 0 for i in range(len(arr1))])
+
 
 lst1 = MathList([10,20,30])
 print(lst1)
@@ -48,3 +48,4 @@ print(lst1 + lst2)
 print(lst2 - lst1)
 print(lst1 * lst2)
 print(lst1//lst2)
+print(lst1)
---
+++ 1 - Python-2/22/from lesson.py
@@ -0,0 +1,135 @@
+arr1 = [ 1,2,3,4,5]
+arr2 = [1,2,3]
+for i in range(len(arr2)):
+    print(arr1[i], arr2[i])
+
+
+for x,y in zip(arr1, arr2):
+    print(x, y)
+
+for pair in zip(arr1, arr2):
+    print(pair)
+
+s1 = "abcde"
+arr = [1,2,3,4,5]
+for pair in zip(s1, arr):
+    print(pair)
+
+ar1 = [1, 2, 3, 4]
+ar2 = "abc"
+ar3 = ("x", "y", "z")
+for pair in zip(ar1, ar2, ar3):
+    print(pair)
+
+print(list(enumerate("hello"))) # [(0, 'h'), (1, 'e'), (2, 'l'), (3, 'l'), (4, 'o')]
+print(list(zip(ar1, ar2, ar3))) # [(1, 'a', 'x'), (2, 'b', 'y'), (3, 'c', 'z')]
+
+x = dict([("a", 1), ("b", 2), ("c", 3)])
+print(x) # {'a': 1, 'b': 2, 'c': 3}
+
+a1 = ("z", "x", "y")
+a2 = [1, 2, 3]
+dct = dict(zip(a1, a2))
+print(dct) # {'z': 1, 'x': 2, 'y': 3}
+
+s = "hello"
+length = len(s)
+for pair in zip(range(length), s):
+    print(pair)
+
+# ===============================================
+person = ("Jack", "123-23-34")
+print(f"The person is {person[0]} with phone number {person[1]}")
+
+
+class Person:
+    def __init__(self, name, phone_number):
+        self.name = name
+        self.phone_number = phone_number
+
+p1 = Person("Jack", "123-23-34")
+print(p1.name)
+
+#============================
+from collections import namedtuple
+
+Person = namedtuple("Person", ["name", "phone_number"])
+user1 = Person(name="Jack", phone_number="123-333-44")
+print(user1) # Person(name='Jack', phone_number='123-333-44')
+print(type(user1)) # <class '__main__.Person'>
+print(isinstance(user1, tuple)) # True
+print(user1.phone_number)
+print(f"The person is {user1.name} with phone number {user1.phone_number}")
+
+print(user1[0]) # Jack
+print(user1[1]) # 123-333-44
+print(len(user1)) # 2
+
+Person = namedtuple("Person", ["name", "phone_number", "age"])
+user1 = Person(name="Jack", phone_number="123-333-44", age=37)
+print(user1.age)
+test_user = Person("Alice", "212-111-11", 20)
+
+user1 = Person(test_user[0], test_user[1], test_user[2])
+print(user1)
+
+# =============================
+arr = [1, 2, 3, 4, 5]
+print(1, 2, 3, 4, 5)
+
+def add(x, y):
+    return  x + y
+
+
+def func(*iterable):
+    print(iterable) #
+    print(iterable[:4])
+
+
+func(1,2,3,4,5)  # (1, 2, 3, 4, 5)  (1, 2, 3, 4)
+# ------------------------------
+def func(*iterable):
+    return sum(iterable)
+
+
+print(func(1,2,3,4,5)) # 15
+#--------------------------------
+def func(x, y, *iterable):
+    return sum(iterable)
+
+
+print(func(1,2,3,4,5)) # 12
+# =================================
+print(1,2,3,4,5, end=";") #keyword argument # 1 2 3 4 5;
+
+def func(x, y, *iterable,**kwargs):
+    print(x,y)
+    print(iterable)
+    print(kwargs)
+
+print("-------------")
+func(10,20, 30, 40, my_args="hello", text="bla-bla")
+
+print(*[1,2,3,4]) # 1 2 3 4 (типа спред-оператор - превращает массив в набор элементов
+
+
+# ----------
+test_dct = {"name": "Bob",
+             "phone_number": "12345",
+             "age": 23}
+
+user2 = Person(name="Jack", phone_number="123-333-44", age=37)
+print(user2)
+user3 = Person(**test_dct) # распакованный словарь
+print(user3)
+
+print(f"The person is {user1.name} with phone number {user1.phone_number}. He is {user1.age} years old")
+
+# ===========
+from collections import Counter
+c = Counter()
+c["a"] = 10
+print(c["a"])  # 10
+del c["a"]
+print(c)
+print(isinstance(c, dict))
