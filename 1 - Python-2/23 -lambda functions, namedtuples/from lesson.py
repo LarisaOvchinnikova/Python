252f9e2fa174f2a5312bab851a395d619cb2b042
Larisa <larisaplantation@gmail.com>
2020-07-23 19:55:48 -0400

    HW22

---
+++ 1 - Python-2/23 -lambda functions, namedtuples/from lesson.py
@@ -0,0 +1,25 @@
+days = 'Monday Tuesday Wednesday Thursday Friday Saturday Sunday'.split()
+
+print(days)
+print(sorted(days, key=len)) # sort by length of words
+
+
+days = [('Monday', 'Chest+biceps', '45'),
+        ('Tuesday', 'Back+triceps', '45'),
+        ('Wednesday', 'Core', '30'),
+        ('Thursday', 'Legs', '55'),
+        ('Friday', 'Shoulders', '45'),
+        ('Saturday', 'Rest', '0'),
+        ('Sunday', 'Rest', '0')]
+
+def get_third(element):
+    return element[2]
+
+print(sorted(days,key=get_third))   # сортировка по посл.элементу таплс
+
+def get_second(element):
+    return element[1]
+
+print(sorted(days,key=get_second))   # сортировка по 2 элементу таплс
+
+
