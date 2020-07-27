74db21705e18877b8c714d8ba6c164b85189eed8
Larisa <larisaplantation@gmail.com>
2020-07-27 11:34:36 -0400

    codewars

--- codewars python/if else/Recursion 101.py
+++ codewars python/if else/Recursion 101.py
@@ -1,18 +1,14 @@
+https://www.codewars.com/kata/5b752a42b11814b09c00005d/train/python
 def solve(a,b):
-    if a == 0 or b == 0:
-        print(f"#1 {a},{b}")
+    def solve(a, b):
+        if not (a and b):
+            return [a, b]
+        if a >= 2 * b:
+            a = a % (2 * b)
+            return solve(a, b)
+        if b >= 2 * a:
+            b = b % (2 * a)
+            return solve(a, b)
         return [a, b]
-    elif a >= 2*b:
-        print(f"#2 {a},{b}")
-        a = a - 2*b
-        solve(a, b)
-    elif b >= 2*a:
-        print(f"#3 {a},{b}")
-        b = b - 2*a
-        solve(a, b)
-    else:
-        print(f"#4 {a},{b}")
-        return [a, b]
-
 
 print(solve(6, 19))
