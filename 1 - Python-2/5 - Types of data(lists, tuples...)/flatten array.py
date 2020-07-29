eb2dec4269c61a65e00d06448ae5022dd25159ab
Larisa <larisaplantation@gmail.com>
2020-07-28 23:29:21 -0400

    flatten elements

--- 1 - Python-2/5 - Types of data(lists, tuples...)/flatten array.py
+++ 1 - Python-2/5 - Types of data(lists, tuples...)/flatten array.py
@@ -7,3 +7,14 @@ def flatten(arr):
             result.append(elem)
     return result
 print(list(flatten(x)))
+# 2 case^
+# x = ['a', 'b', ['c', 'd', [[0, "1", ["3"]]]]]
+# def flatten(arr):
+#     for elem in arr:
+#         if type(elem) in [list, tuple]:
+#             yield from flatten(elem)
+#         else:
+#             yield elem
+# print(list(flatten(x)))
+
+# yield from будет возвращать по одному элементу из списка
