21e44f9bbcf22f2fb9a4987d8c678ac29e8583ba
Larisa <larisaplantation@gmail.com>
2020-08-06 11:58:10 -0400

    codewars

---
+++ codewars python/dictionaries(objects)/Find all non-consecutive numbers.py
@@ -0,0 +1,8 @@
+#https://www.codewars.com/kata/58f8b35fda19c0c79400020f/train/python
+def all_non_consecutive(arr):
+    res = []
+    for i in range(1, len(arr)):
+        if arr[i] != arr[i - 1] + 1:
+            res.append({"i": i, "n": arr[i]})
+    return res
+
