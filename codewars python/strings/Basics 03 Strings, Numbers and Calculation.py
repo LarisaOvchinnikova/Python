f4fbf068de16390004464969194bdf522e06c7de
Larisa <larisaplantation@gmail.com>
2020-08-21 11:41:01 -0400

    codewars

---
+++ codewars python/strings/Basics 03 Strings, Numbers and Calculation.py
@@ -0,0 +1,21 @@
+# https://www.codewars.com/kata/56b5dc75d362eac53d000bc8/train/python
+def calculate_string(st):
+    s = ""
+    for el in st:
+        if el.isdigit() or el in ".+-*/":
+            s = s + el
+    for i, el in enumerate(s):
+        if el in "+-*/":
+            oper = i
+    num1 = float(s[:oper])
+    num2 = float(s[oper+1:])
+    op = s[oper]
+    if op == "+":
+        res = num1 + num2
+    elif op == "-":
+        res = num1 - num2
+    elif op == "*":
+        res = num1 * num2
+    elif op == "/":
+        res = num1 / num2
+    return str(round(res))
