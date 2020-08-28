70eaddd890b2b40313c2273275ca6cb46da3a90a
Larisa <larisaplantation@gmail.com>
2020-08-28 16:22:20 -0400

    codewars

---
+++ codewars python/loops/Complete The Pattern n.1.py
@@ -0,0 +1,9 @@
+#https://www.codewars.com/kata/5572f7c346eb58ae9c000047/train/python
+def pattern(n):
+    s = '';
+    for i in range(1, n+1):
+        for j in range(1, i+1):
+            s = s + str(i)
+        if i < n:
+            s = s + "\n"
+    return s
