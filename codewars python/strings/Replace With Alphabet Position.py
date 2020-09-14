4e79f766f678ca657a39ee1e454dffe551a4b128
Larisa <larisaplantation@gmail.com>
2020-09-14 11:08:32 -0400

    codewars

--- codewars python/strings/Replace With Alphabet Position.py
+++ codewars python/strings/Replace With Alphabet Position.py
@@ -2,3 +2,9 @@
 def alphabet_position(text):
     text = text.lower()
     return " ".join([str(ord(c) - 96) for c in text if c.isalpha()])
+
+#2 case
+
+def alphabet_position(text):
+    alph = " abcdefghijklmnopqrstuvwxyz"
+    return " ".join([str(alph.index(el)) for el in text.lower() if el.isalpha()])
