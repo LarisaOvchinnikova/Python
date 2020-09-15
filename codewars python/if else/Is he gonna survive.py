67cf5b61e6c38a37ac0b1da19a343b48e431e1c6
Larisa <larisaplantation@gmail.com>
2020-09-15 00:52:03 -0400

    codewars

--- codewars python/if else/Is he gonna survive.py
+++ codewars python/if else/Is he gonna survive.py
@@ -1,3 +1,4 @@
+https://www.codewars.com/kata/59ca8246d751df55cc00014c
 def hero(bullets, dragons):
     if dragons * 2 <= bullets:
         return True
--- codewars python/if else/Jenny's secret message.py
+++ codewars python/if else/Jenny's secret message.py
@@ -1,3 +1,4 @@
+https://www.codewars.com/kata/55225023e1be1ec8bc000390
 def greet(name):
     if name == 'Johnny':
         return "Hello, my love!"
