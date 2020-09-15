11c35746a08160ac170869abfd2e31cc04e67039
Larisa <larisaplantation@gmail.com>
2020-09-15 16:43:38 -0400

    codewars

--- 1 - Python-2/11 - dictionaries/5 -  Letter occurrences per word.py
+++ 1 - Python-2/11 - dictionaries/5 -  Letter occurrences per word.py
@@ -1,3 +1,4 @@
+import operator
 def find_occurrences(text, letter):
     text = text.lower()
     letter = letter.lower()
@@ -5,5 +6,13 @@ def find_occurrences(text, letter):
     dct = {word:word.count(letter) for word in arr}
     return dct
 
-print(find_occurrences("Hello World", "o"))
-print(find_occurrences("An APPLE a day keeps an Archeologist AWAY...", "A") )
+# print(find_occurrences("Hello World", "o"))
+# print(find_occurrences("An APPLE a day keeps an Archeologist AWAY...", "A") )
+def letter_frequency(text):
+    text = text.lower().replace(" ", "")
+    dct = {letter:text.count(letter) for letter in text}
+    # arr = sorted(list(dct.items()), key=lambda el: el[1], reverse=True)
+    arr = sorted(list(dct.items()), key=operator.itemgetter(1))[::-1]
+    print(arr)
+
+print(letter_frequency('wklv lv d vhfuhw phvvdjh'))
