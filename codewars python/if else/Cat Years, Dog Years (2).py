a88e3fc4ee3d0034ba9ea2a7910004c7a4298cf8
Larisa <larisaplantation@gmail.com>
2020-09-16 17:54:37 -0400

    codewars

--- codewars python/if else/Cat Years, Dog Years (2).py
+++ codewars python/if else/Cat Years, Dog Years (2).py
@@ -14,3 +14,9 @@ def owned_cat_and_dog(cat_years, dog_years):
     else:
         dog = 2 + (dog_years - 24) // 5
     return [cat, dog]
+
+# 2 case
+def owned_cat_and_dog(cy, dy):
+    cat = 0 if cy < 15 else 1 if cy < 24 else 2 + (cy - 24) // 4
+    dog = 0 if dy < 15 else 1 if dy < 24 else 2 + (dy - 24) // 5
+    return [cat, dog]
