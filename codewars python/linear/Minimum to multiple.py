d01a9eacbc40e0595b7de0c29b60855d7b56cef8
Larisa <larisaplantation@gmail.com>
2020-09-19 11:22:24 -0400

    codewars

---
+++ codewars python/linear/Minimum to multiple.py
@@ -0,0 +1,7 @@
+https://www.codewars.com/kata/5e030f77cec18900322c535d
+def minimum(a, x):
+    div_before = a // x
+    rem = a % x
+    div_aft = div_before + 1
+    rem_plus = x * div_aft - a
+    return min(rem, rem_plus)
