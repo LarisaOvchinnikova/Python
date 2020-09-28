c0c9bf03fcaee3cd7488b0ef1674b1e93917d19b
Larisa <larisaplantation@gmail.com>
2020-09-28 17:55:37 -0400

    codewars

--- codewars python/arrays/Take a Ten Minute Walk.py
+++ codewars python/arrays/Take a Ten Minute Walk.py
@@ -1,3 +1,4 @@
+# https://www.codewars.com/kata/54da539698b8a2ad76000228
 def is_valid_walk(walk):
   if len(walk) == 10 and walk.count("n") == walk.count("s") and walk.count("w") == walk.count("e"):
       return True
