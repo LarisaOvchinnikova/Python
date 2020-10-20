218356be5406ebefe0722bf2be77641ae22203f7
Larisa <larisaplantation@gmail.com>
2020-10-19 23:27:38 -0400

    codewars

--- codewars python/strings/Character with longest consecutive repetition.py
+++ codewars python/strings/Character with longest consecutive repetition.py
@@ -14,3 +14,18 @@ def longest_repetition(chars):
         else:
             m = 1
     return (s, maxi)
+
+# 2 case
+def longest_repetition(chars):
+    if chars == "":
+        return '', 0
+    s = chars[0]
+    for i in range(1, len(chars)):
+        if chars[i] == s[-1]:
+            s +=chars[i]
+        else:
+            s += ' '+ chars[i]
+    s = s.split()
+    m = max([len(el) for el in s])
+    x = [el for el in s if len(el) == m]
+    return x[0][0], m
