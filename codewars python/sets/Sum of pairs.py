e313030b501c20740b3a6aa478c878ca4481c054
Larisa <larisaplantation@gmail.com>
2020-10-30 23:16:43 -0400

    codewars

--- codewars python/sets/Sum of pairs.py
+++ codewars python/sets/Sum of pairs.py
@@ -5,3 +5,12 @@ def sum_pairs(lst, s):
         if s - i in uniq:
             return [s - i, i]
         uniq.add(i)
+
+# 2 case:
+def sum_pairs(ints, s):
+    dct = {}
+    for el in ints:
+        if (s-el) in dct:
+            return [s-el, el]
+        else:
+            dct[el] = 1
