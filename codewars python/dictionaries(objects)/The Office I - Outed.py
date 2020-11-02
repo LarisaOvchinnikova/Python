13a83e157d3272bd3f0c3333fd85d9bfc536178e
Larisa <larisaplantation@gmail.com>
2020-11-02 17:54:57 -0500

    codewars

---
+++ codewars python/dictionaries(objects)/The Office I - Outed.py
@@ -0,0 +1,7 @@
+https://www.codewars.com/kata/57ecf6efc7fe13eb070000e1/
+def outed(meet, boss):
+    s = meet[boss]
+    for name in meet:
+        s+= meet[name]
+    n = len(list(meet.keys()))
+    return 'Get Out Now!' if s / n <= 5 else 'Nice Work Champ!'
