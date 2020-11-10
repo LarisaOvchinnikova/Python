eb4eb5b4c70a26d299e8786dd1d54829ef4825f3
Larisa <larisaplantation@gmail.com>
2020-11-10 09:44:04 -0500

    set functions

--- tasks for students/GUI.py
+++ tasks for students/GUI.py
@@ -2,8 +2,19 @@ from tkinter import *
 from tkinter import ttk
 
 def add():
+    result.delete(0, END)
     result.insert(0, int(num1.get()) + int(num2.get()))
 
+def diff():
+    result.delete(0, END)
+    result.insert(0,int(num1.get()) - int(num2.get()))
+
+def mult():
+    result.delete(0, END)
+    result.insert(0,int(num1.get()) * int(num2.get()))
+def div():
+    result.delete(0, END)
+    result.insert(0,int(num1.get()) / int(num2.get()))
 
 root = Tk()
 root.title("Sum")
@@ -12,12 +23,15 @@ mainframe.grid(column=3, row=3)
 
 # создадим окна для ввода чисел
 num1 = Entry(root)
-num1.grid(row=0, column=1)
+num1.grid(row=0, column=1, columnspan=2)
 
 num2 = Entry(root)
-num2.grid(row=0, column=2)
+num2.grid(row=0, column=3, columnspan=2)
 
-ttk.Button(root, text="Sum", command=add).grid(row=2, column=0, columnspan=3)
+ttk.Button(root, text="+", command=add).grid(row=2, column=1)
+ttk.Button(root, text="-", command=diff).grid(row=2, column=2)
+ttk.Button(root, text="*", command=mult).grid(row=2, column=3)
+ttk.Button(root, text="/", command=div).grid(row=2, column=4)
 
 # создадим окно для вывода результата
 result = Entry(root, width=33)
