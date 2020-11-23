e7c1e4f7a43cec26ddfd0b22e041e0706a3188da
Larisa <larisaplantation@gmail.com>
2020-11-23 11:07:36 -0500

    codewars

--- codewars python/arrays/Swap the head and the tail.py
+++ codewars python/arrays/Swap the head and the tail.py
@@ -1,4 +1,4 @@
-https://www.codewars.com/kata/5a34f087c5e28462d9000082
+# https://www.codewars.com/kata/5a34f087c5e28462d9000082
 def swap_head_tail(arr):
     if len(arr) % 2 == 0:
         p1 = arr[:len(arr)//2]
@@ -8,3 +8,8 @@ def swap_head_tail(arr):
         p1 = arr[:len(arr)//2]
         p2 = arr[len(arr)//2+1:]
         return p2 + [arr[len(arr)//2]] + p1
+
+#2 case
+def swap_head_tail(arr):
+    i = len(arr) // 2
+    return arr[-i:] + arr[i:-i] + arr[:i]
