fdfbb5446aba9f552ea15bce21fe0f6ee4594092
Larisa <larisaplantation@gmail.com>
2020-12-08 11:53:54 -0500

    codewars

--- codewars python/list comprehension/Convert Color image to greyscale.py
+++ codewars python/list comprehension/Convert Color image to greyscale.py
@@ -5,3 +5,7 @@ def color_2_grey(colors):
         arr = [[round(sum(el1)/3)] * 3 for el1 in el]
         res.append(arr)
     return res
+
+# 2 case
+def color_2_grey(colors):
+    return [[[round(sum(el1)/3)] * 3 for el1 in el] for el in colors]
