46a7f5f172b14944fd7655f21bc0d6e83e2e51b3
Larisa <larisaplantation@gmail.com>
2020-12-22 22:21:53 -0500

    codewars

---
+++ codewars python/math/STRONGN Strong Number (Special Numbers Series 2).py
@@ -0,0 +1,16 @@
+# https://www.codewars.com/kata/5a4d303f880385399b000001
+def factorial(n):
+    f = 1
+    for i in range(1, n + 1):
+        f *= i
+    return f
+
+
+def strong_num(number):
+    return 'STRONG!!!!' if sum([factorial(int(el)) for el in str(number)]) == number else 'Not Strong !!'
+
+# 2 case
+import math
+
+def strong_num(number):
+    return "STRONG!!!!" if sum(math.factorial(int(i)) for i in str(number)) == number else "Not Strong !!"
