1ddac5f90fc776ebaaf4a9cf6913e3e8b3e76da5
Larisa <larisaplantation@gmail.com>
2021-02-26 18:03:54 -0500

    Added codewars solution

---
+++ 1 - Python-2/10 - unit tests/HW10(unit testing)/test_cinema.py
@@ -0,0 +1,29 @@
+# import unittest
+#
+#
+# class MyTestCase(unittest.TestCase):
+#     def test_something(self):
+#         self.assertEqual(True, False)
+#
+#
+# if __name__ == '__main__':
+#     unittest.main()
+def cinema(x, y):
+    if x > 2 * y or y > 2 * x: return None
+    s = ''
+    if x >= y:
+        trio = x - y
+        doubles = y - trio
+        s += 'BGB'* trio
+        s += "BG" * doubles
+    else:
+        trio = y - x
+        doubles = x - trio
+        s += 'GBG' * trio
+        s += "GB" * doubles
+    return s
+
+
+def test_cinema():
+    assert cinema(1, 1) == "BG"
+    assert cinema(2, 1) == "BGB"
