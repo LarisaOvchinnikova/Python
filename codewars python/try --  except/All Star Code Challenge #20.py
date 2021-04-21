0621fe4a2e69c576fac2b45098e30fa2f70edc74
Larisa <larisaplantation@gmail.com>
2021-04-21 17:46:54 -0400

    Added codewars solution

--- codewars python/try --  except/All Star Code Challenge #20.py
+++ codewars python/try --  except/All Star Code Challenge #20.py
@@ -3,3 +3,9 @@ def add_arrays(a, b):
     if len(a) != len(b):
         raise Exception
     return [a[i] + b[i] for i in range(len(a))]
+
+# 2 case
+def add_arrays(a, b):
+    if len(a) != len(b):
+        raise ValueError
+    return [a[i] + b[i] for i in range(len(a))]
