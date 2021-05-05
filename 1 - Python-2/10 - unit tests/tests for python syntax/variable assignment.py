7c4c413ebdc98c866908538343b91ba4a4ecd0a2
Larisa <larisaplantation@gmail.com>
2021-05-05 15:22:02 -0400

    Test for variable assignment

---
+++ 1 - Python-2/10 - unit tests/tests for python syntax/variable assignment.py
@@ -0,0 +1,14 @@
+# Name of challenger
+# Variable assignment
+
+
+# Create a variable with the name `number` and assign it a value `10`.
+
+#Open test
+class TestClass(object):
+    def test_1(self):
+        """Variable created"""
+        assert number == 10, f"expected value equal to 10"
+
+#Completed solution
+number = 10
